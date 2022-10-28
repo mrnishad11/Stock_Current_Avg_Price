78ec1b978a1e184ae983348cc4be1c48f1f044f4
Abhishek Nishad <70302971+mrnishad11@users.noreply.github.com>
2022-10-28 09:17:01 +0530

    28_october

--- Avg_Prices.py
+++ Avg_Prices.py
@@ -4,19 +4,18 @@ from nsepy import get_history
 from datetime import datetime
 from openpyxl.styles import Font
 from openpyxl.styles.numbers import FORMAT_PERCENTAGE_00
-start_date=datetime(2022,6,1)
-end_date=datetime(2022,10,17)
-stock=['OBEROIRLTY','TCS']
+date=datetime(2022,10,27)
+stock=['AEGISCHEM','BANKBARODA','CANBK','GUJGASLTD','INDIANB','MAHABANK']
 ans=[]
 for i in stock:
-    stock_data=get_history(symbol=i,start=start_date,end=end_date)
+    stock_data=get_history(symbol=i,start=date,end=date)
     stock_data.to_excel(f'{i}.xlsx')
     stockname=i+'.xlsx'
     wb = openpyxl.load_workbook(stockname)
     ws=wb.active    
 
 #VWAP
-    for rows in range(1,127):
+    for rows in range(1,10):
         copy='J'+str(rows)
         if ws[copy].value==None:
             break
@@ -38,5 +37,6 @@ for i in range(2,len(stock)):
     paste2='B'+str(i)
     ac[paste1]=stock[i]
     ac[paste2]=ans[i]
-d=str(end_date)
-Avg.save(f'Avg_price{d[:10]}.xlsx')    
+d=str(date)
+Avg.save(f'Avg_price_{d[:10]}.xlsx')    
+
